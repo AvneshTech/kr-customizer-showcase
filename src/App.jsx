b2f600de6f2c9bc60c73ca13b8e25a322fa0d192
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-02 02:27:59 +0530

    Initial project structure and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+// import Home from "./pages/Home";
+
+// function App() {
+//   return <Home />;
+// }
+
+// export default App;
+
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <h1 className="text-5xl text-blue-600 text-center mt-20">
-      KR Customizer Showcase
+    <>
+      <h1 className="text-5xl text-blue-600 text-center mt-10">
+        KR Customizer Showcase
+      </h1>
+
       <Home />
-    </h1>
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-center mt-10">
+        Home Page
+      </h1>
+    </main>
+  );
+};
+
+export default Home;
